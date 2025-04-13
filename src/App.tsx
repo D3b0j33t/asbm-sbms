d9703d5724a848f2b5fc316569988fe5a3192e21
Debojeet Bhowmick <161986304+D3b0j33t@users.noreply.github.com>
2025-04-13 11:50:50 +0530

    updt

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-
-import React, { Suspense } from "react";
+import React, { Suspense, useState, useEffect } from "react";
+import { supabase } from '../utils/supabase';
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -49,3 +49,28 @@ const App: React.FC = () => {
 };
 
 export default App;
+
+function Page() {
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    function getTodos() {
+      const { data: todos } = await supabase.from('todos').select()
+
+      if (todos.length > 1) {
+        setTodos(todos)
+      }
+    }
+
+    getTodos()
+  }, [])
+
+  return (
+    <div>
+      {todos.map((todo) => (
+        <li key={todo}>{todo}</li>
+      ))}
+    </div>
+  )
+}
+export default Page
--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = 'https://jvsvvvehimepwnijkmnh.supabase.co';
+const SUPABASE_URL = 'https://msnpgbzkycckmosrjfua.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2c3Z2dmVoaW1lcHduaWprbW5oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQzNzA2ODMsImV4cCI6MjA1OTk0NjY4M30.nJh-9B2zvhX4t8nPpPFyTl0JhKcbK2IxgLOem3mR1s0';
 
 // Import the supabase client like this:
